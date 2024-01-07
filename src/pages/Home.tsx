0d107c8b949a0f01e9c37727ce297d144b92b3a2
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-01-06 22:55:57 -0300

    feat: creating home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
-import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 const Home = () => {
-  return <div className={styles.container}>Home</div>;
+  return (
+    <div className="max-w-1440 mx-auto">
+      <Header />
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
