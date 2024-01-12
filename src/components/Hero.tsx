842cbf389b9f9b73e3d27653eea8bb52206d1f2a
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-01-11 23:45:46 -0300

    feat: importing companys component in hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,11 @@
+import Companys from "./Companys";
+
 const Hero = () => {
   return (
-    <main className="flex items-center justify-between bg-blue-300">
+    <main className="flex items-center justify-between">
       <div className="min-h-screen-3/4 w-full">
-        <div className="w-full min-h-full bg-[url('../assets/image-hero-desktop.png')] bg-cover bg-center bg-no-repeat"></div>
+        {/* bg image div */}
+        <div className=""></div>
       </div>
       <div>
         <h1>Make remote work</h1>
@@ -12,6 +15,9 @@ const Hero = () => {
           soar.
         </p>
         <button>Learn more</button>
+        <div className="companys">
+          <Companys />
+        </div>
       </div>
     </main>
   );
