7e489e94957aac65e68dd147cfdd7ae6eaa2e6be
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-01-11 23:45:25 -0300

    feat: mapping through data file to get companys

--- src/components/Companys.tsx
+++ src/components/Companys.tsx
@@ -1,7 +1,13 @@
+import companys from "../data/companys.json";
+
 const Companys = () => {
   return (
-    <div>
-      <h2>Companys</h2>
+    <div className="flex items-center justify-between py-8">
+      {companys.map((company) => (
+        <div key={company.id}>
+          <img src={company.url} alt="Company" />
+        </div>
+      ))}
     </div>
   );
 };
