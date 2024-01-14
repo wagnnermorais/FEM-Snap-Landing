8781f9f5f91695c8e280067d1f5426b33beb6a0b
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-01-13 23:59:11 -0300

    style: styling desktop hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,13 +2,12 @@ import Companys from "./Companys";
 
 const Hero = () => {
   return (
-    <main className="flex items-center justify-between">
-      <div className="min-h-screen-3/4 w-full">
-        {/* bg image div */}
-        <div className=""></div>
-      </div>
-      <div>
-        <h1>Make remote work</h1>
+    <main className="flex items-center justify-between flex-row-reverse">
+      <div className="min-h-screen-3/4 w-2/4 bg-[url('src/assets/image-hero-desktop.png')] bg-center bg-cover bg-no-repeat"></div>
+      <div className="w-2/4">
+        <h1 className="flex flex-col text-7xl font-bold">
+          Make <span>remote work</span>
+        </h1>
         <p>Get your team in sync, no matter your location.</p>
         <p>
           Streamline processes, create team rituals, and watch productivity
