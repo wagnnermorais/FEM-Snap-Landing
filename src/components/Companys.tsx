1ae355eff44f331bcb66a236d3ab57e93026df2d
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-01-14 22:38:05 -0300

    style: adding gap between images

--- src/components/Companys.tsx
+++ src/components/Companys.tsx
@@ -2,7 +2,7 @@ import companys from "../data/companys.json";
 
 const Companys = () => {
   return (
-    <div className="flex items-center justify-between py-8">
+    <div className="flex items-center justify-between gap-9">
       {companys.map((company) => (
         <div key={company.id}>
           <img src={company.url} alt="Company" />
