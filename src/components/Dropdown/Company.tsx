41d5784ec7fa2a371f7fd4537f3f20e4ee2379c8
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-01-17 12:49:05 -0300

    refac: adding link tag

--- src/components/Dropdown/Company.tsx
+++ src/components/Dropdown/Company.tsx
@@ -1,9 +1,15 @@
 const Company = () => {
   return (
     <menu className="flex flex-col gap-4 mt-4 p-6 bg-white shadow-md rounded-xl absolute z-10 top-full">
-      <li>History</li>
-      <li>Our Team</li>
-      <li>Blog</li>
+      <li>
+        <a href="#">History</a>
+      </li>
+      <li>
+        <a href="#">Our Team</a>
+      </li>
+      <li>
+        <a href="#">Blog</a>
+      </li>
     </menu>
   );
 };
