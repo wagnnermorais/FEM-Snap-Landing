faa740bbc40359853afe0dc126c6838a5023ab16
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-01-17 21:29:01 -0300

    style: reducing gap in mobile screen

--- src/components/Companys.tsx
+++ src/components/Companys.tsx
@@ -2,7 +2,7 @@ import companys from "../data/companys.json";
 
 const Companys = () => {
   return (
-    <div className="flex items-center justify-between gap-9">
+    <div className="flex items-center justify-between md:gap-9 sm:gap-4">
       {companys.map((company) => (
         <div key={company.id}>
           <img src={company.url} alt="Company" />
