695c8bc6d59c949c4ce67862531b4056a75460d2
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-01-18 20:15:59 -0300

    feat: adding mobile menu toggler

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import Navigation from "./Navigation";
+import MobileMenu from "./MobileMenu";
+import ButtonBox from "./ButtonBox";
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+
+  const handleToggleMenu = () => {
+    setOpenMenu((prev) => !prev);
+  };
+
   return (
     <header className="flex items-center justify-between py-8 text-lg text-medium-gray font-medium md:px-0 sm:px-4">
       <div className="flex items-center gap-12">
@@ -13,24 +22,17 @@ const Header = () => {
         </div>
         <Navigation />
       </div>
-      <div className="md:flex items-center gap-12 sm:hidden">
-        <button className="hover:text-almost-black duration-300" type="button">
-          Login
-        </button>
-        <button
-          className="px-6 py-2 border-2 rounded-2xl border-medium-gray hover:border-almost-black hover:text-almost-black duration-300"
-          type="button"
-        >
-          Register
-        </button>
+      <div className="md:block sm:hidden">
+        <ButtonBox />
       </div>
-      <div className="md:hidden sm:block">
+      <div className="md:hidden sm:block" onClick={() => handleToggleMenu()}>
         <img
           src="src/assets/icon-menu.svg"
           alt="Mobile Menu"
           className="cursor-pointer"
         />
       </div>
+      {openMenu && <MobileMenu toggleMenu={handleToggleMenu} />}
     </header>
   );
 };
