ab9d3d3f3a67a9ea1d5a8fd1b008f9c304913e42
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-01-18 20:16:51 -0300

    style: increasing top/right value

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -9,7 +9,7 @@ const MobileMenu = ({ toggleMenu }: MobileMenuProps) => {
   return (
     <div className="flex flex-row-reverse w-full min-h-screen fixed top-0 left-0 bg-black bg-opacity-50 z-50">
       <div className="w-2/3 min-h-screen bg-white relative z-10 p-8">
-        <div className="absolute top-1 right-1" onClick={toggleMenu}>
+        <div className="absolute top-4 right-4" onClick={toggleMenu}>
           <img src="src/assets/icon-close-menu.svg" alt="Close Menu Icon" />
         </div>
         <Navigation />
