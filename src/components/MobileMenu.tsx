d7476b34fd0c25e97f81b96187a166966179bffa
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-01-23 17:57:36 -0300

    feat: adding new prop and menu open logic

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -2,9 +2,11 @@ import ButtonBox from "./ButtonBox";
 import Navigation from "./Navigation";
 import { MobileMenuProps } from "../types/MobileMenuProps";
 
-const MobileMenu = ({ toggleMenu }: MobileMenuProps) => {
+const MobileMenu = ({ toggleMenu, isMenuOpen }: MobileMenuProps) => {
   return (
-    <div className="flex flex-row-reverse w-full min-h-screen fixed top-0 left-0 bg-black bg-opacity-50 z-50">
+    <div
+      className={`flex flex-row-reverse w-full min-h-screen fixed top-0 left-0 -translate-x-full transition duration-300 bg-black bg-opacity-50 z-50 ${isMenuOpen ? "translate-x-0" : ""}`}
+    >
       <div className="w-2/3 min-h-screen bg-white relative z-10 p-8">
         <div className="absolute top-4 right-4" onClick={toggleMenu}>
           <img src="src/assets/icon-close-menu.svg" alt="Close Menu Icon" />
