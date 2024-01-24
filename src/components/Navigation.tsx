22ee1e3d072977d8d706f6f768b274d86ce72ce4
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-01-24 20:17:54 -0300

    refac: changing html structure to adapt dropdown menu

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -25,38 +25,42 @@ const Navigation = () => {
   return (
     <div className="relative lg:m-0 sm:mt-20 sm:mb-6">
       <menu className="lg:flex lg:items-center gap-8 lg:gap-16 sm:block">
-        <li
-          className="flex items-center gap-2 font-semibold ease-out duration-300 hover:text-almost-black relative lg:m-0 sm:mb-4"
-          onClick={() => handleMenuDrop("features")}
-        >
-          <a href="#">Features</a>
-          <img
-            src={
-              isFeaturesDropdownOpen
-                ? "/icon-arrow-up.svg"
-                : "/icon-arrow-down.svg"
-            }
-            alt="Arrow Icon"
-            className="mt-0.5 cursor-pointer"
-          />
-        </li>
-        {isFeaturesDropdownOpen && <Features />}
-        <li
-          className="flex items-center gap-2 font-semibold ease-out duration-300 hover:text-almost-black relative lg:m-0 sm:mb-4"
-          onClick={() => handleMenuDrop("company")}
-        >
-          <a href="#">Company</a>
-          <img
-            src={
-              isCompanyDropdownOpen
-                ? "/icon-arrow-up.svg"
-                : "/icon-arrow-down.svg"
-            }
-            alt="Arrow Icon"
-            className="mt-0.5 cursor-pointer"
-          />
-        </li>
-        {isCompanyDropdownOpen && <Company />}
+        <div>
+          <li
+            className="flex items-center gap-2 font-semibold ease-out duration-300 hover:text-almost-black relative lg:m-0 sm:mb-4"
+            onClick={() => handleMenuDrop("features")}
+          >
+            <a href="#">Features</a>
+            <img
+              src={
+                isFeaturesDropdownOpen
+                  ? "/icon-arrow-up.svg"
+                  : "/icon-arrow-down.svg"
+              }
+              alt="Arrow Icon"
+              className="mt-0.5 cursor-pointer"
+            />
+          </li>
+          <div>{isFeaturesDropdownOpen && <Features />}</div>
+        </div>
+        <div>
+          <li
+            className="flex items-center gap-2 font-semibold ease-out duration-300 hover:text-almost-black lg:m-0 sm:mb-4"
+            onClick={() => handleMenuDrop("company")}
+          >
+            <a href="#">Company</a>
+            <img
+              src={
+                isCompanyDropdownOpen
+                  ? "/icon-arrow-up.svg"
+                  : "/icon-arrow-down.svg"
+              }
+              alt="Arrow Icon"
+              className="mt-0.5 cursor-pointer"
+            />
+          </li>
+          <div>{isCompanyDropdownOpen && <Company />}</div>
+        </div>
         <li className="font-semibold ease-out duration-300 hover:text-almost-black lg:m-0 sm:mb-4">
           <a href="#">Careers</a>
         </li>
