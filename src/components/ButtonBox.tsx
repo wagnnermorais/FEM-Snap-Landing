5a0676057766d61b6dacf9f4afa9bd0775380e1f
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-01-24 13:31:52 -0300

    style: reducing button width on medium screen

--- src/components/ButtonBox.tsx
+++ src/components/ButtonBox.tsx
@@ -2,13 +2,13 @@ const ButtonBox = () => {
   return (
     <div className="md:flex items-center gap-12 sm:block">
       <button
-        className="hover:text-almost-black duration-300 sm:my-2 sm:w-full"
+        className="hover:text-almost-black duration-300 sm:my-2 md:w-1/2 sm:w-full"
         type="button"
       >
         Login
       </button>
       <button
-        className="px-6 py-2 border-2 rounded-2xl border-medium-gray hover:border-almost-black hover:text-almost-black duration-300  sm:my-2 sm:w-full"
+        className="px-6 py-2 border-2 rounded-2xl border-medium-gray hover:border-almost-black hover:text-almost-black duration-300  sm:my-2 md:w-1/2 sm:w-full"
         type="button"
       >
         Register
